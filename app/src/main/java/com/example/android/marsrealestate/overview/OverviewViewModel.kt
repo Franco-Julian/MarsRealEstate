a44c5347412329a3ea381805fc4e85ebfc6ddce3
JuliF <julifranco1993@hotmail.com>
2022-03-06 20:14:15 -0300

    Add GridView

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -29,18 +29,25 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.await
 
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
 
     // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
+
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+
+    val properties : LiveData<List<MarsProperty>>
+        get()= _properties
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -54,10 +61,11 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsRealEstateProperties() {
         viewModelScope.launch {
             try {
-                val listResult = MarsApi.retrofitService.getProperties()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
-            } catch (e: Exception){
-                _response.value = "Failure: ${e.message}"
+                _properties.value = MarsApi.retrofitService.getProperties()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
     }
